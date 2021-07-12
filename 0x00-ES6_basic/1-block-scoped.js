ddb622510fbe5e148d649f3dcb594a61750b4ef8
Adam <arbimer@mac.com>
2021-07-12 13:27:24 -0500

    Fix for 1?

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     task = true;
     task2 = false;
   }
-  var task = true;
-  var task2 = false;
+  var task = false;
+  var task2 = true;
   return [task, task2];
 }
