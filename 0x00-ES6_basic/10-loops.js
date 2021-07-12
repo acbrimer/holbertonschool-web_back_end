81cd652769fc25c99d478e77f80b36c69c3037a1
Adam <arbimer@mac.com>
2021-07-12 13:33:46 -0500

    Fix for 10?

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -3,7 +3,7 @@ export default function appendToEachArrayValue(array, appendString) {
     for (const idx of array) {
       const value = array[idx];
       // eslint-disable-next-line no-param-reassign
-      array[idx] = appendString + value;
+      array[idx] = `${appendString || ''}${value || ''}`;
     }
   }
   return array;
