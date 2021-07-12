265367f3c32b52db5e423f6048ecacfbdfd4b8ee
Adam <arbimer@mac.com>
2021-07-12 12:57:45 -0500

    Fix for 100-- using flatMap instead of map/reduce

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,6 @@
 export default function createIteratorObject(report) {
   if (report && Object.keys(report).length > 0) {
-    Object.keys(report)
-      .map((k) => report[k].allEmployees)
-      .reduce((c, acc) => acc.append(...c), []);
+    return Object.keys(report.allEmployees).flatMap((k) => report.allEmployees[k]);
   }
+  return [];
 }
