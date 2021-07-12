1068f002939264c65b1fc06d35b1568a40b7f72a
Adam <arbimer@mac.com>
2021-07-12 12:10:17 -0500

    eslint fix for 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default () => {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = function add(newNeighborhood) {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+};
