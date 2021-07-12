0bc5cfa01f451b31f1a3dcae51fe29ba249f1b8d
Adam <arbimer@mac.com>
2021-07-12 13:25:17 -0500

    Fix for 1?

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-var */
+/* eslint-disable no-use-before-define */
+/* eslint-disable vars-on-top */
 export default function taskBlock(trueOrFalse) {
-  var task = trueOrFalse;
-  var task2 = !trueOrFalse;
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  } else {
+    task = false;
+    task2 = true;
+  }
+  var task;
+  var task2;
   return [task, task2];
 }
