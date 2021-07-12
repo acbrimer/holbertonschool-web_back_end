2fb1d71a9e0931aaefcd518c9f266ee12b13ad57
Adam <arbimer@mac.com>
2021-07-12 12:07:18 -0500

    Fix for 0?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -7,11 +7,9 @@ export function getLast() {
   return ' is okay';
 }
 
-let taskNext = () => {
+export const taskNext = () => {
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 };
-
-export default taskNext;
