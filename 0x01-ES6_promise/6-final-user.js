68e1ab21072ac63674b41ce0a8edb8c40ebd207c
Adam <arbimer@mac.com>
2021-07-15 11:50:56 -0500

    Fix for 6-- reason.message

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,12 +8,12 @@ export default function handleProfileSignup(firstName, lastName, filename) {
     {
       status: values[0].status,
       value:
-        values[0].status === 'fulfilled' ? values[0].value : values[0].reason,
+        values[0].status === 'fulfilled' ? values[0].value : values[0].reason.message,
     },
     {
       status: values[1].status,
       value:
-        values[1].status === 'fulfilled' ? values[1].value : values[1].reason,
+        values[1].status === 'fulfilled' ? values[1].value : values[1].reason.message,
     },
   ]);
 }
