d3fd3d11e7373bd8af2cbcc7e800527713e4316f
Adam <arbimer@mac.com>
2021-07-15 11:45:01 -0500

    Fix for 3-- missing return-- thanks Rachel

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const create = createUser();
-  Promise.all([upload, create])
+  return Promise.all([upload, create])
     .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
     .catch(() => console.error('Signup system offline'));
 }
