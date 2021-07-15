2c26215c6cc826f3bb9799e017f252ace40477c9
Adam <arbimer@mac.com>
2021-07-15 13:04:30 -0500

    Fix for 100-- returning both object props as null if either promise rejected

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.allSettled([uploadPhoto(), createUser()]).then((vals) => ({
-    photo: vals[0].status === 'fulfilled' ? vals[0].value : null,
-    user: vals[1].status === 'fulfilled' ? vals[1].value : null,
-  }));
+  return Promise.allSettled([uploadPhoto(), createUser()])
+    .then((vals) => (vals.map((v) => v.status).includes('rejected')
+      ? ({ photo: null, user: null })
+      : ({
+        photo: vals[0].value,
+        user: vals[1].value,
+      })
+    ));
 }
