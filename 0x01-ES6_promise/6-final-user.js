bc2b0e9dcd76a861d67a00606a2f476c7fb54ba3
Adam <arbimer@mac.com>
2021-07-15 11:37:14 -0500

    Fix for 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   const signup = signUpUser(firstName, lastName);
   const upload = uploadPhoto(filename);
-  Promise.allSettled([signup, upload]).then((values) => values);
+  return Promise.allSettled([signup, upload]).then((values) => [
+    values[0],
+    values[1],
+  ]);
 }
