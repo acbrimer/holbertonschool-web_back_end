fcaae77adfb1b01ceb462c8e05eb1d94cefa3f7e
Adam <arbimer@mac.com>
2021-07-15 12:55:27 -0500

    Fix for 100-- using allSettled to check status of promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()]).then((vals) => ({
-    photo: vals[0],
-    user: vals[1],
+  return Promise.allSettled([uploadPhoto(), createUser()]).then((vals) => ({
+    photo: vals[0].status === 'fulfilled' ? vals[0].value : null,
+    user: vals[1].status === 'fulfilled' ? vals[1].value : null,
   }));
 }
