f2a79235f8804b8e6ef5fb473d1049ee56f9cd5a
Adam <arbimer@mac.com>
2021-07-15 11:47:01 -0500

    Fix for 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,15 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const signup = signUpUser(firstName, lastName);
   const upload = uploadPhoto(filename);
   return Promise.allSettled([signup, upload]).then((values) => [
-    { status: values[0].status, value: values[0].value },
-    { status: values[1].status, value: values[1].value },
+    {
+      status: values[0].status,
+      value:
+        values[0].status === 'fulfilled' ? values[0].value : values[0].reason,
+    },
+    {
+      status: values[1].status,
+      value:
+        values[1].status === 'fulfilled' ? values[1].value : values[1].reason,
+    },
   ]);
 }
