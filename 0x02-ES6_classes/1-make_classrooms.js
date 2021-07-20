b0396eae3a85038e9e40b661dca74b689536067f
Adam <arbimer@mac.com>
2021-07-19 22:39:26 -0500

    Fix for 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [19, 20, 34].map((s) => new ClassRoom[s]());
+  return [19, 20, 34].map((s) => new ClassRoom(s));
 }
