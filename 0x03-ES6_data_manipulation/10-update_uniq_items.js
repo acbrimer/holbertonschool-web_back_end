305add08f12a00df84099431c6dd306577373411
Adam <arbimer@mac.com>
2021-07-21 21:58:32 -0500

    Fix for 10-- not returning new Map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,5 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  return new Map([...map.entries()].map((i) => [i[0], i[1] === 1 ? 100 : i[1]]));
+  [...map.entries()].forEach((i) => {
+    if (i[1] === 1) {
+      map.set(i[0], 100);
+    }
+  });
 }
