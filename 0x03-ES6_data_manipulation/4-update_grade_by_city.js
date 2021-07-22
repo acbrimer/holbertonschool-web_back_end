b496c9e16ce9d655263b68b5d7d50863c2c9f897
Adam <arbimer@mac.com>
2021-07-21 20:55:37 -0500

    Added solution for task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const newGradesObj = newGrades.reduce((acc, c) => ({ ...acc, [c.id]: c.grade }), {});
+  return students.filter((f) => f.city === city).map((s) => (
+    {
+      id: s.id,
+      firstName: s.firstName,
+      grade: newGradesObj[s.id] ? newGradesObj[s.id] : 'N/A',
+    }
+  ));
+}
