18f1195fc186374d223d19df31e1c4ecc268dfb6
Adam <arbimer@mac.com>
2021-07-21 21:23:50 -0500

    Fix for task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const newGradesObj = newGrades.reduce((acc, c) => ({ ...acc, [c.id]: c.grade }), {});
+  const newGradesObj = newGrades.reduce((acc, c) => ({ ...acc, [c.studentId]: c.grade }), {});
   return students.filter((f) => f.location === city).map((s) => (
     {
       id: s.id,
       firstName: s.firstName,
-      grade: newGradesObj[s.id] ? newGradesObj[s.id] : 'N/A',
+      grade: Object.keys(newGradesObj).includes(s.id) ? newGradesObj[s.id] : 'N/A',
     }
   ));
 }
