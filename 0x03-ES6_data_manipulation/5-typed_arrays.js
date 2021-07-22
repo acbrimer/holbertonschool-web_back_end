1897e33fe56444a5787c67e9cf130028c08b27a8
Adam <arbimer@mac.com>
2021-07-21 21:20:04 -0500

    Fix for task 5-- handling out of range

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const buff = new ArrayBuffer(length);
   const view = new Int8Array(buff);
   view[position] = value;
