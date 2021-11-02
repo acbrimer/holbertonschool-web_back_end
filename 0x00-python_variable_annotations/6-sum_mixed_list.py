78e233f8bda7b7ad0faccb8b976e33c469e7acf3
Adam <arbimer@mac.com>
2021-11-02 13:53:15 -0700

    Fix for 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Module for 6-sum_mixed_list """
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ Function sum_mixed_list - sums a mixed list of ints & floats """
     return sum(mxd_lst)
