2b9922f67ac50736deb10523ffddd861679d56da
Adam <arbimer@mac.com>
2021-11-02 13:59:20 -0700

    Added task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module for 7-to_kv """
-from typing import Union, Tuple
+from typing import Union, Tuple, cast
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Function to_kv - returns tuple with k and v^2 """
-    return (k, v ^ 2)
+    return (k, cast(v ^ 2, float))
