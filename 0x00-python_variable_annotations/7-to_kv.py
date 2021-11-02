b92a1b011aaa8baa1dc0652702dbf0467a795cbe
Adam <arbimer@mac.com>
2021-11-02 13:57:36 -0700

    Added task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Function to_kv - returns tuple with k and v^2 """
-    return [k, v ^ 2]
+    return (k, v ^ 2)
