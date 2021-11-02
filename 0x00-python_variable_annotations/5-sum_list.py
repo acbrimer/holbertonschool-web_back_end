1c797aaf0b7d598f6ae74e88bd5d9cd5461f99ea
Adam <arbimer@mac.com>
2021-11-02 13:42:02 -0700

    Added solution for task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module for 5-sum_list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Function sum_list - returns sum of list of floats """
+    return sum(input_list)
