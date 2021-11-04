b3b192693a809788886efbcb743f9fdc46e110fe
Adam <arbimer@mac.com>
2021-11-04 15:49:25 -0500

    Initial commit for 0x02 with solution for task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for 0-async_generator """
+import asyncio
+from random import uniform
+
+
+async def async_generator():
+    """ Function async_generator - generates a random list """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
