2438d1e899f4bc2430c7aa72ba3fcfc883927929
Adam <arbimer@mac.com>
2021-11-04 15:39:17 -0500

    Fix for task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,6 @@ from random import uniform
 
 async def wait_random(max_delay: Optional[int] = 10) -> float:
     """ Function wait_random - waits for random time """
-    random: float = uniform(0, max_delay)
-    await asyncio.sleep(random)
-    return random
+    rand: float = uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
