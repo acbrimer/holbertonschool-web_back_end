65cff35bbbb9efa0ac22c10ebabb493461dc20cf
Adam <arbimer@mac.com>
2021-11-04 15:51:43 -0500

    Fix for task 0-- added annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Module for 0-async_generator """
 import asyncio
 from random import uniform
+from typing import List
 
 
-async def async_generator() -> list[float]:
+async def async_generator() -> List[float]:
     """ Function async_generator - generates a random list """
     for i in range(10):
         await asyncio.sleep(1)
