86f4a5cee84777eef5c104ba690e2a57a9e0a874
Adam <arbimer@mac.com>
2021-11-04 16:25:44 -0500

    Added task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Function wait_n - calls wait_random n times """
-    return await asyncio.gather(
-        *[wait_random(i) for i in range(n)]
-    )
+    return sorted(await asyncio.gather(
+        *[wait_random(max_delay) for i in range(n)]
+    ))
