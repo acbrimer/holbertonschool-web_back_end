7096f96d2b53081f6f92e9898a0a23b5bbe90eb9
Adam <arbimer@mac.com>
2021-11-04 16:56:21 -0500

    Added solution for task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Module for 2-measure_runtime """
 import asyncio
-
+from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """ measure_runtime - calls async_comprehension x4, returns exec time """
-    res = await asyncio.gather(
-        *[async_comprehension() for i in range(4)]
-    )
-    return sum(sum(res, []))
+    start: float = time()
+    await asyncio.gather(
+        *[async_comprehension() for i in range(4)])
+    return (time() - start)
