e6dbb82d29534d228f9c66f628bb5f13761811f2
Adam <arbimer@mac.com>
2021-11-10 12:15:03 -0600

    Fix for 0

--- 0x00-caching/0-basic_cache.py
+++ 0x00-caching/0-basic_cache.py
@@ -7,10 +7,12 @@ class BasicCache(BaseCaching):
     """ BasicCache - simple cache class inherited from BaseCaching """
 
     def put(self, key, item):
+        """ put - save item to cache """
         if (key or item is not None):
             self.cache_data[key] = item
 
     def get(self, key):
+        """ get - retrieve item from cache """
         if (key is None or key not in self.cache_data.keys()):
             return None
         return self.cache_data[key]
