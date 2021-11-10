c6431d0cb5aeec3f0458d596af411f3727069194
Adam <arbimer@mac.com>
2021-11-10 13:02:16 -0600

    Added solution for task 2

---
+++ 0x00-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module for 1-fifo_cache """
+from base_caching import BaseCaching
+import collections
+
+
+class LIFOCache(BaseCaching):
+    """ FIFOCache - first in first out cache """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = collections.OrderedDict()
+
+    def put(self, key, item):
+        """ put - save item to cache """
+        if (not key or not item):
+            return
+        if (len(self.cache_data.keys()) == self.MAX_ITEMS):
+            discard = self.cache_data.popitem(last=True)
+            print('DISCARD: {}'.format(discard[0]))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get - retrieve item from cache """
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        return self.cache_data[key]
