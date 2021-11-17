f7e3da0481748f55a7e1b725dc45e357fc676d63
Adam <arbimer@mac.com>
2021-11-17 13:51:48 -0600

    Fix- add annotation

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields, redaction, message, separator) -> str:
     """ filter_datum - was asked to use re and, technically, I did  """
     rows = [s.split("=") for s in message.split(separator)][:-1]
     return separator.join(["{}={}".format(
