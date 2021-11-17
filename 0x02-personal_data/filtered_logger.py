c7cb2330fce057c4cddb759043c7f6304d202883
Adam <arbimer@mac.com>
2021-11-17 13:51:17 -0600

    Fix- removed print for 0

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -6,7 +6,6 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """ filter_datum - was asked to use re and, technically, I did  """
     rows = [s.split("=") for s in message.split(separator)][:-1]
-    print(rows)
     return separator.join(["{}={}".format(
         r[0],
         re.sub(str(r[1]), str(redaction), r[1]) if r[0] in fields else r[1])
