3f0d8444500ab7418c65b54ef4c3b0055e7819bb
Adam <arbimer@mac.com>
2021-11-17 21:38:48 -0600

    Added task 1

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -2,6 +2,8 @@
 """ Module for filter_datum """
 import re
 from typing import List
+import logging
+import time
 
 
 def filter_datum(fields: List[str],
@@ -14,3 +16,26 @@ def filter_datum(fields: List[str],
         re.sub(r[1], redaction, r[1]) if r[0] in fields else r[1])
         for r in [s.split("=") for s in message.split(separator)][:-1]]),
         separator)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.FLDS = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format - formatter for logging """
+        t = time.asctime(time.localtime())
+        m = filter_datum(self.FLDS, self.REDACTION, record.msg, self.SEPARATOR)
+        d = dict(zip(['name', 'levelname', 'asctime', 'message'],
+                     [record.name, record.levelname, t, m]))
+        record.msg = self.FORMAT % d
+
+        return super(RedactingFormatter, self).format(record)
