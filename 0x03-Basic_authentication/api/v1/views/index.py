9afdfbd18c9dc8b13193a42101bfdcdbe4ce6205
Adam <arbimer@mac.com>
2021-11-22 17:33:14 -0600

    Added method for task 11

--- 0x03-Basic_authentication/api/v1/views/index.py
+++ 0x03-Basic_authentication/api/v1/views/index.py
@@ -32,7 +32,7 @@ def unauthorized():
     Return:
       - returns a 401 abort error
     """
-    abort(401)
+    return abort(401)
 
 
 @app_views.route('/unauthorized/')
@@ -41,4 +41,4 @@ def forbidden():
     Return:
       - returns a 403 forbidden error
     """
-    abort(403)
+    return abort(403)
