5153580e9f50dcfb539fab8a6ac522e4b1800624
Adam <arbimer@mac.com>
2021-11-22 16:46:41 -0600

    Added basic_auth for task 6

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth_type = os.getenv("AUTH_TYPE")
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 excluded_paths = ['/api/v1/status/',
                   '/api/v1/unauthorized/', '/api/v1/forbidden/']
