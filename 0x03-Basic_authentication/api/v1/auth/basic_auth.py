c47ab274a3b3c5d24433ca3f58ad2429a1f4298a
Adam <arbimer@mac.com>
2021-11-22 16:51:37 -0600

    Added method for task 7

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract_base64_authorization_header
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic'):
+            return None
+        if not ' ' in authorization_header:
+            return None
+        return authorization_header.split(' ')[1]
