7ca896058481b59e73e48bfcb45526f290208abf
Adam <arbimer@mac.com>
2021-11-22 16:24:23 -0600

    Added auth for task 4

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -4,14 +4,22 @@ from flask import request
 from typing import List, TypeVar
 
 
+def clean_url(url: str) -> str:
+    return url[:-1] if url[-1] == '/' else url
+
+
 class Auth:
     """ Auth class
         Handles authentication to API
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth - hardcoded to return False """
-        return False
+        """ require_auth
+            Returns True if path is None
+            Returns True if excluded_paths is None or empty
+            Returns False if path is in excluded_paths
+        """
+        return clean_url(path) in [clean_url(p) for p in excluded_paths]
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header - hardcoded None """
