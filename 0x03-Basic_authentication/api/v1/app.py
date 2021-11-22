822edd7aa6c388b27bfebcb5b279fda01c8e7618
Adam <arbimer@mac.com>
2021-11-22 16:41:35 -0600

    Added auth methods for task 5

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,7 +15,6 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.environ['AUTH_TYPE']
 if auth:
-    from api.v1.auth.auth import Auth
     auth = Auth(auth)
 
 excluded_paths = ['/api/v1/status/',
