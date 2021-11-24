cada1ebab46515d5f1ca33a3c0952d9171e58fde
Adam <arbimer@mac.com>
2021-11-24 15:21:07 -0600

    Added solution for task 2

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """ Module for session_auth """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """ SessionAuth 
-    Manages session-based authentication
-    """
-    pass
+    """ SessionAuth """
+    user_id_by_session_id = dict({})
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create_session - creates a Session ID for user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
