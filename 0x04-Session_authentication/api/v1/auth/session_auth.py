1ad7a006782eae9db6c5a7b15127da02f36d3b82
Adam <arbimer@mac.com>
2021-11-24 15:40:17 -0600

    Added task 6

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module for session_auth """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,9 @@ class SessionAuth(Auth):
         if session_id is None:
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """ current_user - overload to return User from cookie val """
+        session_id_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id_cookie)
+        return User.get(user_id)
