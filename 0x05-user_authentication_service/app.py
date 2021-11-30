193e5dd9fd08abc9e5f812b8ee0e9205292d074a
Adam <arbimer@mac.com>
2021-11-30 14:12:17 -0600

    Added profile route to flask app for task 15

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -48,5 +48,17 @@ def logout():
     abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """ Returns JSON for user tied to session_id cookie """
+    sessionId = request.cookies.get('session_id', None)
+    if not sessionId:
+        abort(403)
+    user = AUTH.get_user_from_session_id(sessionId)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
