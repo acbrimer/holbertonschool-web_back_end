cd66ed85df2fe244c13bbc4a04c41ea6d1e1e0c6
Adam <arbimer@mac.com>
2021-11-30 13:14:06 -0600

    Added solution for task 2

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -1,10 +1,11 @@
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -35,3 +36,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """ Finds user by fields in kwargs """
+        for key, value in kwargs.items():
+            if key not in User.__table__.columns:
+                raise InvalidRequestError()
+        res = self._session.query(User).filter_by(**kwargs).first()
+        if not res:
+            raise NoResultFound()
+        return res
