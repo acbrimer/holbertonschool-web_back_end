59a1246758b9c3ef0bc140b4cf5678c7fcb26d28
Adam <arbimer@mac.com>
2021-11-30 19:01:28 -0600

    Fix for user

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module for user model """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
