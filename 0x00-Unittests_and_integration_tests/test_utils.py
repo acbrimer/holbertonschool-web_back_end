7e0ce370e082fddad032b4aec087a0dcc2eaa05b
Adam <arbimer@mac.com>
2021-12-07 14:38:47 -0600

    Added solution for task 2

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ Tests access_nested_map function """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        [{}, ("a", ), 'a'],
+        [{"a": 1}, ("a", "b"), 'b']
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """ Tests access_nested_map function raises correct execption """
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+            self.assertEqual(str(err.exception),
+                             "KeyError: '{}'".format(expected_key))
