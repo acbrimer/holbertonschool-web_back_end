e04790721dce91ccae7cad5d76acc8292270512c
Adam <arbimer@mac.com>
2021-12-13 13:27:34 -0600

    Initial commit for 0x01-i18n w/ solution for task 0

---
+++ 0x01-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for 0-app """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """ index - returns the index template """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
