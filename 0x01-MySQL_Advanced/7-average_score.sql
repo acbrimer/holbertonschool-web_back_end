9e4acb45a9499b85f05e51de54a4a8885521c05f
Adam <arbimer@mac.com>
2021-12-16 12:48:26 -0600

    Added solution for task 7

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -1,25 +1,23 @@
--- Creates proc to compute avg score for users
+-- Procedure to compute avg score for users
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser 
 	(
 	IN user_id int
 	)
 	BEGIN
-		-- aggregate avg scores in CTE
-		WITH avg_cte
-		AS
-		(
+		
+		-- join to aggregation sub-query to set avg
+		UPDATE users AS u
+		INNER JOIN (
 			SELECT
 				c.user_id
 				, AVG(c.score) AS `average_score`
 			FROM corrections AS c
 			WHERE c.user_id = user_id
 			GROUP BY c.user_id
-		)
-		-- join to CTE to update users
-		UPDATE users AS u
-		INNER JOIN avg_cte AS a
+		) AS a
 			ON u.id = a.user_id
 		SET u.average_score = a.average_score;
 	END;
 	$$
+	
