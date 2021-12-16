6efa28e80845a016f84e42d86df6ebf4c0cd1211
Adam <arbimer@mac.com>
2021-12-16 13:11:59 -0600

    Added solution for task 11

---
+++ 0x01-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,11 @@
+-- Creates a view for students w/ score < 80 & no meeting within the last month
+CREATE VIEW need_meeting
+AS
+	SELECT
+		*
+	FROM students AS s
+	WHERE s.score < 80
+	AND (
+		s.last_meeting IS NULL
+		OR s.last_meeting < DATE_ADD(NOW(),INTERVAL -1 MONTH)
+		);
