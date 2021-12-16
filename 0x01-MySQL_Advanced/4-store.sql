d3e168ee93301b0b4a44505d0e29a7eb0ebecd29
Adam <arbimer@mac.com>
2021-12-16 11:18:54 -0600

    Added solution for task 4

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,4 +1,6 @@
 -- Creates a trigger on items table
-CREATE TRIGGER trig_decrease_items AFTER INSERT ON items
-FOR EACH ROW SET NEW.amount = NEW.amount - 1;
-
+CREATE TRIGGER trig_decrease_items BEFORE UPDATE ON items
+       FOR EACH ROW
+       BEGIN
+           SET NEW.amount = NEW.amount - 1;
+       END;
