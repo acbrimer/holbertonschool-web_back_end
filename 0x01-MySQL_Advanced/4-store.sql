3987670ae5500681c299c75bd1392cfd6a665256
Adam <arbimer@mac.com>
2021-12-16 11:31:16 -0600

    Added solution for task 4

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -1,8 +1,11 @@
 -- Creates a trigger on items table
-CREATE TRIGGER trig_decrease_items AFTER UPDATE ON orders
+DELIMITER $$
+CREATE TRIGGER trig_decrease_items AFTER INSERT ON orders
        FOR EACH ROW
        BEGIN
            UPDATE items
-          	SET amount = amount - 1
+          	SET quantity = quantity - 1
           	WHERE name = NEW.item_name;
        END;
+$$
+DELIMITER ;
