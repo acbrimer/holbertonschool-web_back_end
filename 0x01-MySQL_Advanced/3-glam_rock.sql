7337d311b8d8b2fe39ee4ef9af1e175db231f83d
Adam <arbimer@mac.com>
2021-12-16 11:10:01 -0600

    Added solution for task 3

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Query to order glam bands by lifespan
 SELECT
 	mb.band_name,
-	COALESCE(mb.split, YEAR(now())) - mb.formed - 1
+	COALESCE(mb.split, 2020) - mb.formed - 1
 		AS `lifespan`
 FROM metal_bands mb 
 WHERE mb.`style` like '%Glam rock%';
