658a9be8af285e1ee7060be36ae9cc22b9ad7d0b
Adam <arbimer@mac.com>
2021-12-16 10:48:35 -0600

    Fix for task 1

--- 0x01-MySQL_Advanced/1-country_users.sql
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
+	name varchar(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
 
