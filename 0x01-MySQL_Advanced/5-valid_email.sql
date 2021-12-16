372652d06d87b401ea18a06e5e9c8252b31225e9
Adam <arbimer@mac.com>
2021-12-16 11:42:06 -0600

    Added solution for task 5

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -3,16 +3,10 @@ DELIMITER $$
 CREATE TRIGGER trig_reset_valid_email BEFORE UPDATE ON users
        FOR EACH ROW
        BEGIN
-	       DECLARE current_email varchar(255)
-
-	       SELECT u.email
-	       INTO @current_email
-	       FROM users AS u
-	       WHERE u.id = NEW.id;
-	       
-	       IF current_email != NEW.email
-	           SET NEW.valid_email = 0;
+	       IF OLD.email != NEW.email
+	       THEN
+	           SET NEW.valid_email = false;
 	       END IF;
        END;
 $$
-DELIMITER ;
+DELIMITER 
