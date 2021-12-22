a641b7efe8af30005b2a68ee6f0ed55078b36ea3
Adam <arbimer@mac.com>
2021-12-21 23:25:44 -0600

    Added solution for task 12

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""Module for 12-log_stats"""
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client.logs
+collection = db["nginx"]
+res = len(list(collection.find()))
+print("{} logs".format(res))
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+pipeline = [
+    {"$match": {"method": {"$in": methods}}},
+    {"$group": {"_id": "$method", "count": {"$sum": 1}}}
+]
+agg = dict([(r['_id'], r['count'])
+           for r in list(collection.aggregate(pipeline))])
+print("Methods:")
+res = ["  method {}: {}".format(
+    m, agg[m] if m in agg.keys() else 0) for m in methods]
+print("\n".join(res))
+res = len(list(collection.find({"path": "/status"})))
+print("{} status check".format(res))
