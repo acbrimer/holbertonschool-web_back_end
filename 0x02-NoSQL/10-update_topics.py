c3b8d319a9c7bd3adc66f307fa395bcbe918ac96
Adam <arbimer@mac.com>
2021-12-21 22:46:58 -0600

    Added solution for task 10

--- 0x02-NoSQL/10-update_topics.py
+++ 0x02-NoSQL/10-update_topics.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" Module for 10-update_topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """update_topics - updates mongo collection"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
