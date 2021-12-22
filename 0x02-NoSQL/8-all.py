1416f75517f9fa5935d4f9661edb641aec7ce374
Adam <arbimer@mac.com>
2021-12-21 22:37:49 -0600

    Added solution for task 8

--- 0x02-NoSQL/8-all.py
+++ 0x02-NoSQL/8-all.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" Module for 8-all """
+
+
+def list_all(mongo_collection):
+    """list_all - lists all docs in mongo_collection"""
+    return [doc for doc in mongo_collection.find()]
