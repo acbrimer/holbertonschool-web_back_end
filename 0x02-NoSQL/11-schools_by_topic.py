7f41a7da853a2414c06a9070ca7a4b743e01aa39
Adam <arbimer@mac.com>
2021-12-21 22:50:20 -0600

    Added solution for task 11

--- 0x02-NoSQL/11-schools_by_topic.py
+++ 0x02-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""11-schools_by_topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """schools_by_topic - lists schools by topic"""
+    return mongo_collection.find({"topics": {"$in": [topic]}})
