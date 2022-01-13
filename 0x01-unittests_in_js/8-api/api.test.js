418ff836bd72a1fcbacff9deaed123cdb2048921
Adam <arbimer@mac.com>
2022-01-12 22:45:19 -0600

    Added solution for task 8

---
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+// 8-api/api.test
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Test Module for 8-api', () => {
+  it('Test index page', (done) => {
+    request('http://localhost:7865/', (err, res, body) => {
+      console.log('res', res);
+      console.log('body', body);
+      done();
+      // Correct status code?
+      expect(res).to.be.not.null;
+      expect(res.statusCode).to.equal(200);
+      // Correct result?
+      expect(err).to.be.null;
+      expect(body).to.be.not.null;
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+});
