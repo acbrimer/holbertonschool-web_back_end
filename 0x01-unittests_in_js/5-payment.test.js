150843168a5dd96b569c2b5a69448cf59902c05e
Adam <arbimer@mac.com>
2022-01-12 22:21:12 -0600

    Added solution for task 5

--- 0x01-unittests_in_js/5-payment.test.js
+++ 0x01-unittests_in_js/5-payment.test.js
@@ -5,6 +5,16 @@ const expect = require('chai').expect;
 const Utils = require('./utils');
 
 describe('Test Module 3-payment', () => {
+  let spy_console;
+
+  beforeEach(() => {
+    spy_console = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy_console.restore();
+  });
+
   it('SUM totalAmount + totalShipping with sendPaymentRequestToApi', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
@@ -14,26 +24,20 @@ describe('Test Module 3-payment', () => {
   });
   it('Check sendPaymentRequestToApi is logging to console', () => {
     const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const spy_console = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
     stub.calledWith(100, 20);
     spy_console.calledWith('The total is: 10');
     stub.returns(10);
     stub.restore();
-    spy_console.restore();
   });
   it('call sendPaymentRequestToAPI with 100, and 20', () => {
-    const spy_console = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
     expect(spy_console.calledOnce).to.be.true;
     expect(spy_console.calledWith('The total is: 120')).to.be.true;
-    spy_console.restore();
   });
   it('call sendPaymentRequestToAPI with 10, and 10', () => {
-    const spy_console = sinon.spy(console, 'log');
     sendPaymentRequestToApi(10, 10);
     expect(spy_console.calledOnce).to.be.true;
     expect(spy_console.calledWith('The total is: 20')).to.be.true;
-    spy_console.restore();
   });
 });
