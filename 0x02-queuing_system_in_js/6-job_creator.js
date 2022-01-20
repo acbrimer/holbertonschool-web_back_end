b302cea58feb607beba7ca8b1202706e33daa781
Adam <arbimer@mac.com>
2022-01-20 13:51:40 -0600

    Added solution for task 6

---
+++ 0x02-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,31 @@
+// 6-job_creator
+import kue from 'kue';
+
+class JobCreator {
+  /**
+   * JobCreator - creates a job an dhandles completion
+   */
+  constructor(queue_name, job_data) {
+    this.que = kue.createQueue();
+    this.job = this.que.create(queue_name, job_data);
+    this.job.save(
+      (err) => !err && console.log(`Notification job created: ${this.job.id}`)
+    );
+    this.job.on('complete', this.handleJobComplete);
+    this.job.on('failed', this.handleJobFailure);
+  }
+
+  handleJobComplete = () => console.log('Notification job completed');
+
+  handleJobFailure = () => console.log('Notification job failed');
+}
+
+const main = () => {
+  const data = {
+    phoneNumber: 123456789,
+    message: 'hello, job queue!',
+  };
+  new JobCreator('push_notification_code', data);
+};
+
+main();
