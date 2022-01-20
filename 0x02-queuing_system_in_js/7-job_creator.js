ddd724c5c2640d77b339498b5e3b44596f6d2571
Adam <arbimer@mac.com>
2022-01-20 14:07:16 -0600

    Added solution for task 8

---
+++ 0x02-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,73 @@
+// 7-job_creator
+import { createQueue } from 'kue';
+
+class JobCreator {
+  /**
+   * JobCreator - creates a job an dhandles completion
+   */
+  constructor(queue_name, job_data) {
+    this.que = createQueue();
+    this.job = this.que.create(queue_name, job_data);
+    this.job.save(
+      (err) => !err && console.log(`Notification job created: ${this.job.id}`)
+    );
+    this.job.on('complete', this.handleJobComplete);
+    this.job.on('failed', this.handleJobFailure);
+  }
+
+  handleJobComplete = () => console.log('Notification job completed');
+
+  handleJobFailure = () => console.log('Notification job failed');
+}
+
+const main = () => {
+  const jobs = [
+    {
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account',
+    },
+    {
+      phoneNumber: '4153518781',
+      message: 'This is the code 4562 to verify your account',
+    },
+    {
+      phoneNumber: '4153518743',
+      message: 'This is the code 4321 to verify your account',
+    },
+    {
+      phoneNumber: '4153538781',
+      message: 'This is the code 4562 to verify your account',
+    },
+    {
+      phoneNumber: '4153118782',
+      message: 'This is the code 4321 to verify your account',
+    },
+    {
+      phoneNumber: '4153718781',
+      message: 'This is the code 4562 to verify your account',
+    },
+    {
+      phoneNumber: '4159518782',
+      message: 'This is the code 4321 to verify your account',
+    },
+    {
+      phoneNumber: '4158718781',
+      message: 'This is the code 4562 to verify your account',
+    },
+    {
+      phoneNumber: '4153818782',
+      message: 'This is the code 4321 to verify your account',
+    },
+    {
+      phoneNumber: '4154318781',
+      message: 'This is the code 4562 to verify your account',
+    },
+    {
+      phoneNumber: '4151218782',
+      message: 'This is the code 4321 to verify your account',
+    },
+  ];
+  jobs.forEach((job) => new JobCreator('push_notification_code_2', job));
+};
+
+main();
